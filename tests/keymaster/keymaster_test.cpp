a01da856067c4a0d57485cb46cf2d41d59440207
Florent Guichard <guichardflorent@gmail.com>
2013-09-12 19:27:52 +0200

    fix mismerge

--- tests/keymaster/keymaster_test.cpp
+++ tests/keymaster/keymaster_test.cpp
@@ -311,15 +311,12 @@ public:
 
         std::cout << "Using keymaster module: " << mod->name << std::endl;
 
-        ASSERT_EQ(0, keymaster_open(mod, &sDevice))
+	ASSERT_EQ(0, keymaster_open(mod, &sDevice))
                 << "Should be able to open the keymaster device";
 
-<<<<<<< HEAD
-=======
         ASSERT_EQ(KEYMASTER_MODULE_API_VERSION_0_2, mod->module_api_version)
                 << "Keymaster should implement API version 2";
 
->>>>>>> 36ab8ed... keymaster: use new version fields in hw_module
         ASSERT_TRUE(sDevice->generate_keypair != NULL)
                 << "Should implement generate_keypair";
 
